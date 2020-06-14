3d3543e5f6d9b65163621a2a599a2806b6b1ee4c
Zacaria <havesomecode@gmail.com>
2020-06-15 00:37:02 +0200

    [UP] install

--- install.sh
+++ install.sh
@@ -1,9 +1,29 @@
 EMAIL=havesomecode@gmail.com
 
-# Generate ssh keu if not exists
+# Generate ssh key if not exists
 if [ ! -f "~/.ssh/id_rsa.pub" ]; then
   ssh-keygen -t rsa -b 4096 -C "$EMAIL"
 fi
+
+# Zsh
+
+brew install zsh
+chsh -s /usr/local/bin/zsh
+
+# Oh my zsh
+
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+cp .zshrc ~
+
+chmod 755 /usr/local/share/zsh
+chmod 755 /usr/local/share/zsh/site-functions
+
+## Theme powerlevel10k
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/themes/powerlevel10k
+
+source ~/.zshrc
+
+p10k configure
 
 # Configure git
 
